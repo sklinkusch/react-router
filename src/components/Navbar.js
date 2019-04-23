eb1ac458b008c9983dbeadef8d47461d4cc17514
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-23 11:11:31 +0200

    made links in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Info</li>
-      <li>Credits</li>
+      <li>
+        <Link to="/home">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/info">Info</Link>
+      </li>
+      <li>
+        <Link to="/credit">Credit</Link>
+      </li>
     </ul>
   )
 }
