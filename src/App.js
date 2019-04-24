30ee6fbcfc7252c46effc8f20fab688fb608bf53
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-24 09:33:39 +0200

    added a not found page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import About from './components/About';
 import Info from './components/Info';
 import Credits from './components/Credits';
 import Home from './components/Home';
 import Topics from './components/Topics';
+import Failure from './components/Failure';
 
 function App() {
   return (
@@ -17,12 +18,15 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Navbar />
-          <Route path="/" exact render={() => <h1>Welcome</h1>} />
-          <Route path="/home" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/info" component={Info} />
-          <Route path="/credits" component={Credits} />
-          <Route path="/topics" component={Topics} />
+          <Switch>
+            <Route path="/" exact render={() => <h1>Welcome</h1>} />
+            <Route path="/home" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/info" component={Info} />
+            <Route path="/credits" component={Credits} />
+            <Route path="/topics" component={Topics} />
+            <Route component={Failure} />
+          </Switch>
         </header>
       </div>
     </BrowserRouter>
---
+++ src/components/Failure.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Failure() {
+  return (
+    <div>
+      <h1>Oops! The page you are looking for does not exist.</h1>
+    </div>
+  )
+}
